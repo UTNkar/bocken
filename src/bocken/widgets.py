78c6e7bb739e8029029de1974bccef3df30e6c41
Daniel Fehrm <daniel@fehrm.se>
2021-02-11 11:42:03 +0100

    Fix correct key values on two level select

--- src/bocken/widgets.py
+++ src/bocken/widgets.py
@@ -10,22 +10,23 @@ class TwoLevelSelect(Select):
     groups to make it more easy to find a specific group. Each group
     has been placed into a main group. When a main group is selected
     the groups that can be selected is limited to the groups belonging
+    to that main group.
     """
 
     template_name = 'two_level_select.html'
 
     def get_context(self, name, value, attrs):
-        """Provide all the groups and all the verbose names for the groups."""
+        """Provide all the groups to the template."""
         context = super(TwoLevelSelect, self).get_context(name, value, attrs)
-        context['groups'] = JOURNAL_ENTRY_ALL_GROUPS
 
-        # Creates arrays with all verbose names for each main group
-        groups_verbose_names = {}
+        # Convert all list of tuples to list of lists since javascript
+        # does not have tuples
+        groups = JOURNAL_ENTRY_ALL_GROUPS
         for key, main_group in JOURNAL_ENTRY_ALL_GROUPS.items():
-            verbose_names = []
+            groups_to_array = []
             for group in main_group['groups']:
-                verbose_names.append(group[1])
-            groups_verbose_names[key] = sorted(verbose_names)
+                groups_to_array.append(list(group))
+            groups[key]['groups'] = groups_to_array
 
-        context['groups_verbose_names'] = groups_verbose_names
+        context['groups'] = groups
         return context

daa9c08cfb4cfff4d92d81fe4f1f0812cdf91eaa
Daniel Fehrm <daniel@fehrm.se>
2021-03-11 11:00:40 +0100

    make journalentry readonly fields only on edit

--- src/bocken/admin.py
+++ src/bocken/admin.py
@@ -97,12 +97,22 @@ class UserAdmin(BaseUserAdmin):
 class JournalEntryAdmin(ModelAdmin):
     """Custom class for the admin pages for journal entry."""
 
-    readonly_fields = ('created', 'get_total_distance')
     list_display = (
         'agreement', 'created', 'group',
         'meter_start', 'meter_stop', 'get_total_distance'
     )
     ordering = ('-created', )
+
+    def get_readonly_fields(self, request, obj=None):
+        """
+        Only display created and get_total_distnace when editing.
+
+        These fields should not be edited, they are there to provide extra
+        information.
+        """
+        if obj:
+            return ('created', 'get_total_distance')
+        return ()
 
 
 class AgreementAdmin(ModelAdmin):

b15df68ad138a74e99f1f1bf77b2892d9a6fb30a
Daniel Fehrm <daniel@fehrm.se>
2021-03-12 16:06:31 +0100

    added main group to journal entry groups

--- src/bocken/models/group.py
+++ src/bocken/models/group.py
@@ -1,7 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class JournalEntryGroup(models.Model):
     name = models.CharField(max_length=60)
+    main_group = models.CharField(
+        max_length=30,
+        choices=(
+            (
+                'committees_workgroups',
+                _("UTN's committees and workgroups")
+            ),
+            ('lg_and_board', _("UTN's management team and board")),
+            ('fum', _("FUM")),
+            ('other_officials', _("Other officials within UTN")),
+            ('sections', _("Sections")),
+            ('cooperations', _("Cooperations")),
+        ),
+        verbose_name=_("Main group")
+    )
     cost_per_mil = models.PositiveIntegerField(default=20)
     starting_fee = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name

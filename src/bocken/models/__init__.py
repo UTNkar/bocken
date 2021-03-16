15acd4beea161f2af7654264a771ebf9ff09f79a
Daniel Fehrm <daniel@fehrm.se>
2021-03-16 17:11:08 +0100

    added site settings model

--- src/bocken/models/__init__.py
+++ src/bocken/models/__init__.py
@@ -3,6 +3,9 @@ from .agreement import Agreement
 from .journal_entry import JournalEntry
 from .group import JournalEntryGroup
 from .report import Report
+from .site_settings import SiteSettings
 
-__all__ = ['Admin', 'Agreement', 'JournalEntry',
-           'JournalEntryGroup', 'Report']
+__all__ = [
+    'Admin', 'Agreement', 'JournalEntry',
+    'JournalEntryGroup', 'Report', 'SiteSettings'
+]

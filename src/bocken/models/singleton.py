0f2cc761f4a3a51d0e900afe0c8dd58859126283
Daniel Fehrm <daniel@fehrm.se>
2021-03-16 18:17:24 +0100

    added documentation on singelton model

--- src/bocken/models/singleton.py
+++ src/bocken/models/singleton.py
@@ -3,26 +3,37 @@ from django.core.cache import cache
 
 
 class SingletonModel(models.Model):
+    """
+    A singleton model only has one row in its table in the database.
+
+    This prevent that multiple rows are created, containing different
+    informtaion which can cause problems and confusion.
+
+    This class uses a cache to decrease the amount of fetches to the database.
+    """
 
     class Meta:
         abstract = True
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs):  # noqa
+        """Save the changes to first row in the database."""
         self.pk = 1
         super(SingletonModel, self).save(*args, **kwargs)
 
         self.set_cache()
 
     def delete(self, *args, **kwargs):
+        """Rows should not be deletable."""
         pass
 
     @classmethod
     def load(cls):
+        """Get the current settings."""
         if cache.get(cls.__name__) is None:
             obj, created = cls.objects.get_or_create(pk=1)
             if not created:
                 obj.set_cache()
         return cache.get(cls.__name__)
 
-    def set_cache(self):
+    def set_cache(self):  # noqa
         cache.set(self.__class__.__name__, self)

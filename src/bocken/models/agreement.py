0bd198d14885f68b541f73fcf9fa584837244af6
Daniel Fehrm <daniel@fehrm.se>
2021-04-27 14:40:21 +0200

    added renewal reminder function

--- src/bocken/models/agreement.py
+++ src/bocken/models/agreement.py
@@ -6,6 +6,8 @@ from django.utils.translation import gettext_lazy as _
 from django.utils.timezone import now, timedelta
 from django.utils.html import format_html
 from django.template.defaultfilters import date
+from django.core.mail import send_mass_mail
+from django.conf import settings
 
 
 class Agreement(models.Model):
@@ -90,6 +92,32 @@ class Agreement(models.Model):
         else:
             return self.expires
     expires_colored.admin_order_field = 'expires'
+
+    @staticmethod
+    def send_renewal_reminder_10_days_left():
+        """Send an email to all agreements that expire in 10 days."""
+        agreements = Agreement.objects.filter(
+            expires=now() + timedelta(days=10)
+        )
+
+        emails = agreements.exclude(email=None).values_list('email', flat=True)
+
+        if len(emails) > 0:
+            subject = _("Reminder to update your Bocken agreement")
+            message = _(
+                "This is an automated message from UTN:s journal system for "
+                "Bocken. You are receiving this email because you have a "
+                "Bocken agreement that will expire in 10 days. If you want "
+                "to continue driving Bocken you must contact UTN:s "
+                "klubbmästare by replying to this email.\n\n"
+                "If you do not want to continue driving Bocken, you can "
+                "ignore this email."
+            )
+            message_tuple = \
+                subject, message, settings.KLUBBMASTARE_EMAIL, list(emails)
+            send_mass_mail(
+                (message_tuple, )
+            )
 
     def save(self, *args, **kwargs):  # noqa
         self.personnummer = format_personnummer(self.personnummer)

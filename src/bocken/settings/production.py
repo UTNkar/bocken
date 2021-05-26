89aaa2762f8f31ef50ee9fd14f037ec248d43293
Daniel Fehrm <daniel@fehrm.se>
2021-05-26 10:21:33 +0200

    Update production.py

--- src/bocken/settings/production.py
+++ src/bocken/settings/production.py
@@ -9,11 +9,6 @@ DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('DJANGO_SECRET')
 
-# Base URL to use when referring to full URLs within the Wagtail admin
-# backend - e.g. in notification emails. Don't include '/admin' or a
-# trailing slash
-BASE_URL = 'https://bocken.utn.se'
-
 ALLOWED_HOSTS = ['.utn.se']
 
 CSRF_COOKIE_SECURE = True

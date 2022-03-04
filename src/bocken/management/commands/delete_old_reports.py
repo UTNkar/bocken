e9555bf5fae3075aa9256aeb916d0124c4d8431e
Daniel Fehrm <daniel@fehrm.se>
2022-03-04 14:26:53 +0100

    linting

--- src/bocken/management/commands/delete_old_reports.py
+++ src/bocken/management/commands/delete_old_reports.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand
 from bocken.models import Report
 
-class Command(BaseCommand):
+
+class Command(BaseCommand):  # noqa
     help = 'Delete old reports'
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **options):  # noqa
         Report.delete_old_reports()
--- src/bocken/management/commands/send_renewal_reminders.py
+++ src/bocken/management/commands/send_renewal_reminders.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand
 from bocken.models import Agreement
 
-class Command(BaseCommand):
+
+class Command(BaseCommand):  # noqa
     help = 'Send automatic renewal reminders'
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **options):  # noqa
         Agreement.send_renewal_reminder_10_days_left()

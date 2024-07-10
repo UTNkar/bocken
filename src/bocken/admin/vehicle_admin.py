b6b20422fb0fdd7d550b421d5fdd4c2d60017de8
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2024-07-10 15:39:24 +0200

    wip: typo

--- src/bocken/admin/vehicle_admin.py
+++ src/bocken/admin/vehicle_admin.py
@@ -3,7 +3,7 @@ from django.contrib.admin import ModelAdmin
 
 
 class VehicleAdmin(ModelAdmin):
-    """Custom class for the admin pages for JournalEntryGroup."""
+    """Custom class for the admin pages for Vehicle."""
 
     list_display = ("vehicle_name", "car", "bike", "vehicle_meter_start", "vehicle_meter_stop")
     list_filter = ['vehicle_name']
